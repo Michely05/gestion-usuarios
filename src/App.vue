321487597059fc4f365ccf1c2d0c2999a311f84a
Michely05 <michelyparedes@gmail.com>
2024-09-16 09:52:37 +0200

    fix user connection between components

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,11 @@
 <script setup>
 import { RouterView } from 'vue-router';
-import UserList from './components/UserList.vue';
-import UserForm from './components/UserForm.vue';
 </script>
 
 
 <template>
   <div id="app">
     <h1>Gestión de usuarios</h1>
-    <UserList />
-    <UserForm />
     <router-view></router-view>
   </div>
 </template>
